7fd5abaf44fa6f937e0a48113e10918b81f427e3
Sri <srima45singh@gmail.com>
2024-01-09 10:25:40 +0530

    AIML programs

---
+++ pgm6.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+# load data from CSV
+data = pd.read_csv('tennisdata.csv')
+print("THe first 5 values of data is :\n",data.head())
+# obtain Train data and Train output
+X = data.iloc[:,:-1]
+print("\nThe First 5 values of train data is\n",X.head())
+y = data.iloc[:,-1]
+print("\nThe first 5 values of Train output is\n",y.head())
+# Convert then in numbers
+le_outlook = LabelEncoder()
+X.Outlook = le_outlook.fit_transform(X.Outlook)
+le_Temperature = LabelEncoder()
+X.Temperature = le_Temperature.fit_transform(X.Temperature)
+le_Humidity = LabelEncoder()
+X.Humidity = le_Humidity.fit_transform(X.Humidity)
+le_Windy = LabelEncoder()
+X.Windy = le_Windy.fit_transform(X.Windy)
+print("\nNow the Train data is :\n",X.head())
+le_PlayTennis = LabelEncoder()
+y = le_PlayTennis.fit_transform(y)
+print("\nNow the Train output is\n",y)
